4b7afb18713a4cd0d3175ade31ca3a3e84fc152a
Zachary Inkster <z.j.inkster@gmail.com>
2021-02-01 09:59:05 -0600

    Made changes to Register.cshtml.cs to add functionality for individual user roles upon sign up.

---
+++ GMTK_Capstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GMTK_Capstone.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GMTK_Capstone.Areas.Identity.IdentityHostingStartup))]
+namespace GMTK_Capstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
